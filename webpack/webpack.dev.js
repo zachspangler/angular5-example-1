87e65bab19d174dbfc8fe5311a07437463ef7320
George Kephart <Gkephart@cnm.edu>
2017-11-14 13:01:55 -0700

    refactored the webpack.dev.js to not use the proxy API

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,7 +2,6 @@ var webpackMerge = require("webpack-merge");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var commonConfig = require("./webpack.common.js");
 var helpers = require("./helpers");
-var targetUrl = require("./target.js");
 
 module.exports = webpackMerge(commonConfig, {
 	devtool: "cheap-module-eval-source-map",
@@ -16,17 +15,5 @@ module.exports = webpackMerge(commonConfig, {
 
 	plugins: [
 		new ExtractTextPlugin("[name].css")
-	],
-
-	devServer: {
-		contentBase: helpers.root("public_html"),
-		historyApiFallback: true,
-		stats: "minimal",
-		proxy: {
-			"/api": {
-				target: targetUrl(),
-				secure: false
-			}
-		}
-	}
+	]
 });
